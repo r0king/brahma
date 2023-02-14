7e87dbcba9259f0da2494e9871c3fc743ec38baf
gimploo <41667550+gimploo@users.noreply.github.com>
2023-02-14 16:14:04 +0530

    moved font settings from main to highlight

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        sixcap: ["'Six Caps'", ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
