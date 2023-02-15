9fb3f4b13c0f6f9e874d3c5953225c1ae870e307
gimploo <gokulkrishnan949@gmail.com>
2023-02-15 10:18:46 +0530

    Create Highlights.jsx

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+const Highlights = () => {
+    return (
+        <div class='my-8'>
+            <div class='p-2'>
+                <img
+                    class='w-full h-full md:w-2/3 md:h-1/3'
+                    src='https://33.media.tumblr.com/4d9aa44ac1be00dd18d030574828b427/tumblr_ngp8u5TiBf1u1m3cdo1_500.gif' />
+            </div>
+            <div>
+                <label>HIGHLIGHTS</label>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Iste nesciunt incidunt explicabo hic natus odio velit nemo ad voluptas doloribus
+                    voluptatem, expedita fugiat consequuntur! In repellendus ullam cupiditate veniam nemo.</p>
+            </div>
+            highlight
+        </div>
+    )
+}
+
+export default Highlights;
