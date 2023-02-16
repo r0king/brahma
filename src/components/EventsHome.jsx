2abb411f6bd5dcffeb50f90c420ddfa513b906e1
17072001MUSAFIR <musafirky@gmail.com>
2023-02-16 10:04:52 +0530

    EVents

--- src/components/EventsHome.jsx
+++ src/components/EventsHome.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function EventsHome() {
     return (
         <div>
-            <div>
+            <div className=''>
                 <div className='relative h-screen'>
                     <div className='font-morganite absolute left-0 bottom-0 md:top-0 text-pri-color text-[40vw] md:text-[21vw] pl-1'>
                         <div className='leading-[0.75]'>OUR</div>
@@ -13,8 +13,19 @@ export default function EventsHome() {
                         <div className='leading-[0.75] pt-10 ml-[23vw]'>Franco Arza & Clément Roche Amelie Maia Victor Costa Simon D’haenens Claudio Guglieri Adriano Esteves Rhodi Iliadou & Peter Ha Filippo Cipriani Isabel Moranta Pablo Stanley</div>
                         <div className='leading-[0.75] ml-[41vw] pt-1'>Josh Kirk Diana Varma Vitaly Friedman Femke van Schoonhoven Nicolas Brassard-Ferron Hilary Hayes Romain Prache Travis Neilson Brent David Freaney Jonathan Morin</div>
                     </div>
-                    <div className='font-poppins absolute text-sec-color text-[23px] md:text-[2.75rem] font-bold -bottom-[2vw] right-0'>
+                    <div className='font-poppins absolute text-sec-color text-[23px] md:text-[2.75rem] font-bold bottom-0  md:bottom-[17vw] right-0'>
                         (+30)
+                    </div>
+                </div>
+                <div className='relative'>
+                    <div className='absolute'>
+                        <article>
+                            <figure></figure>
+                            <main>
+                                <h2>Vitaly Friedman</h2>
+                                <div></div>
+                            </main>
+                        </article>
                     </div>
                 </div>
             </div>
