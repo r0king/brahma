70051e4be20446fe0e04ac2cf0620c53149719bb
noobiewe <amaanumer1@gmail.com>
2023-02-16 13:42:00 +0530

    sponsors

---
+++ src/componets/Sponsors.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Sponsors() {
+    return (
+        <div className='bg-[#212121] text-white font-normal p-10'>
+            Special Thanks To All Our Partners
+            <div className=' flex flex-wrap'>
+                <div className='ml-10 border-[1px] border-[#fafafa3d] p-36 mt-10 w-10 aspect-square rounded-tr-3xl' >
+                    test
+                </div>
+            </div>
+        </div>
+    )
+}
