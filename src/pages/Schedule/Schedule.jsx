87abbee26a044f760b580819e5981dee8c9e2bd7
Abhijith Jaideep <abhijithjaideep176@gmail.com>
2023-02-17 20:34:12 +0530

    made the page responsive

--- src/pages/Schedule/Schedule.jsx
+++ src/pages/Schedule/Schedule.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './schedule.css'
 
 const Schedule = () => {
@@ -6,72 +6,75 @@ const Schedule = () => {
     const [image, setImage] = useState(-1)
 
 
-   
+
     const sched = [
         {
-        id: 0,
-        timing: "09:00 - 16:00",
-        category: "buisness",
-        img: "https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        description: "How to create and sell digital collectibles with no code",
-        personnel: "Pablo Stanley"
-    },
-    {
-        id: 1,
-        timing: "10:00 - 15:00",
-        category: "Fasion",
-        img: "https://images.pexels.com/photos/39866/entrepreneur-startup-start-up-man-39866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        description: "How to create and sell digital collectibles with no code",
-        personnel: "larry"
-    },
-    {
-        id: 2,
-        timing: "09:00 - 16:00",
-        category: "buisness",
-        img:"https://images.pexels.com/photos/445109/pexels-photo-445109.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        description: "How to create and sell digital collectibles with no code",
-        personnel: "Pablo Stanley"
-    },
-    {
-        id: 3,
-        timing: "09:00 - 16:00",
-        category: "buisness",
-        img:"https://images.pexels.com/photos/2102416/pexels-photo-2102416.jpeg",
-        description: "How to create and sell digital collectibles with no code",
-        personnel: "Pablo Stanley"
-    }
+            id: 0,
+            timing: "09:00 - 16:00",
+            category: "buisness",
+            img: "https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            description: "How to create and sell digital collectibles with no code",
+            personnel: "Pablo Stanley"
+        },
+        {
+            id: 1,
+            timing: "10:00 - 15:00",
+            category: "Fasion",
+            img: "https://images.pexels.com/photos/39866/entrepreneur-startup-start-up-man-39866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            description: "How to create and sell digital collectibles with no code",
+            personnel: "larry"
+        },
+        {
+            id: 2,
+            timing: "09:00 - 16:00",
+            category: "buisness",
+            img: "https://images.pexels.com/photos/445109/pexels-photo-445109.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            description: "How to create and sell digital collectibles with no code",
+            personnel: "Pablo Stanley"
+        },
+        {
+            id: 3,
+            timing: "09:00 - 16:00",
+            category: "buisness",
+            img: "https://images.pexels.com/photos/2102416/pexels-photo-2102416.jpeg",
+            description: "How to create and sell digital collectibles with no code",
+            personnel: "Pablo Stanley"
+        }
     ]
 
     return (
-        <div className='schedule'>
-            <table className='sched-table' cellPadding={15}>
-                <tbody>
-                    {
-                        sched.map((item) => {
-                            console.log("helo")
-                            return (
+        <div className='wrapper'>
+            <h2>Schedule</h2>
+            <div className='schedule'>
+                <div className='sched-table'>
+                    <tbody>
+                        {
+                            sched.map((item) => {
+                                console.log("helo")
+                                return (
 
-                                <tr key={item.id} onMouseEnter={()=>{setImage(item.id)}} onMouseLeave={()=>{setImage(-1)}}>
-                                    <td className='timing'>{item.timing}</td>
-                                    <td className='description'>
-                                        <li className='category'>{item.category}</li>
-                                        <li>{item.description}</li>
-                                    </td>
-                                    <td>{item.personnel}</td>
-                                </tr>)
+                                    <li className='row' key={item.id} onMouseEnter={() => { setImage(item.id) }} onMouseLeave={() => { setImage(-1) }}>
+                                        <li className='timing'>{item.timing}</li>
+                                        <li className='description'>
+                                            <li className='category'>{item.category}</li>
+                                            <li>{item.description}</li>
+                                        </li>
+                                        <li className='personnel'>{item.personnel}</li>
+                                    </li>)
+                            }
+
+                            )
                         }
 
-                        )
-                    }
 
+                    </tbody>
+                </div>
 
-                </tbody>
-            </table>
+                <div className="images">
+                    {image !== -1 && <img className='person-image' src={sched[image].img} alt="" />}
+                </div>
 
-            <div className="images">
-                {image!==-1&&<img className='person-image' src={sched[image].img} alt="" />}
             </div>
-
         </div>
     )
 }
