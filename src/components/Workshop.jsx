35903e715d82ea3c3ab7d95f317bd1d7245f12ef
Sid9021 <sidharthkajith9021@gmail.com>
2023-02-18 14:57:36 +0530

    added carousel and overall layout need hover effects & infinte scroll

--- src/components/Workshop.jsx
+++ src/components/Workshop.jsx
@@ -6,7 +6,7 @@ import P4 from '../assets/images/pic4.jpg'
 import P5 from '../assets/images/pic5.jpeg'
 import P6 from '../assets/images/pic6.jpeg'
 import P7 from '../assets/images/pic7.jpeg'
-import '../App.css'
+import CardButton from './CardButton'
 
 
 const Workshop = () => {
@@ -14,24 +14,12 @@ const Workshop = () => {
     <div className='mt-10'>
       <div className='flex flex-col md:flex-row'>
         <h2 className='leading-[0.75] text-[150px] font-medium tracking-normal font-morganite text-pri-color uppercase md:text-[25vw]'>WORKSHOPS</h2>
-        <div class='relative self-end group overflow-hidden transition-all md:mx-10 md:h-1/3 h-[7.5em] md:w-[20em] w-screen mb-4 bg-red-600 rounded-lg'>
-          <span className="w-full h-full rounded-lg bg-orange-500 absolute top-0 bottom-0 ease-out 
-                        duration-300 transition-all group-hover:h-0 -z-0" />
-          <span class='transition-colors duration-100 ease-in-out text-white stroke-white z-10 
-                        relative rounded-lg p-2 flex justify-between w-full '>
-            <div class='text-left'>
-              <label class='text-xs font-medium mb-2'>Workshops</label>
-              <div class='mt-4 flex flex-col items-stretch text-2xl font-semibold'>
-                <label class=''>View</label>
-                <label class=''>All Workshops</label>
-              </div>
-            </div>
-            <svg class='absolute m-2 bottom-0 right-0' width="34px" height="34px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M16.2928932,7 L11.5,7 C11.2238576,7 11,6.77614237 11,6.5 C11,6.22385763 11.2238576,6 11.5,6 L17.5,6 C17.7761424,6 18,6.22385763 18,6.5 L18,12.5 C18,12.7761424 17.7761424,13 17.5,13 C17.2238576,13 17,12.7761424 17,12.5 L17,7.70710678 L6.85355339,17.8535534 C6.65829124,18.0488155 6.34170876,18.0488155 6.14644661,17.8535534 C5.95118446,17.6582912 5.95118446,17.3417088 6.14644661,17.1464466 L16.2928932,7 Z"></path> </g></svg>
-          </span>
+        <div class=' mr-24 w-1/4 h-[8vw] self-end'>
+          <CardButton href='/' />
         </div>
       </div>
 
-      
+
       <div className="carousel carousel-center w-full md:h-[50vw] h-[90vw] p-4 space-x-4 bg-neutral">
         <div className="carousel-item min-w-[30vw] md:max-w-[50vw]">
           <div className='flex flex-col'>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import ContactUs from "../components/ContactUs";
 import Hero from "../components/Hero";
-import NavBar from "../components/NavBar";
+import Navbar from "../components/Navbar";
 // import Highlights from "../components/Highlights";
 // import Highlight from "../components/Highlight";
 import EventsHome from "../components/EventsHome";
@@ -12,13 +12,12 @@ export default class HomePage extends Component {
   render() {
     return (
       <div className="text-pri-color ">
-        <NavBar />
+        <Navbar />
         <Hero />
         <Workshop/>
         {/* <Highlights /> */}
         {/* <SpotLight/> */}
         <EventsHome />
-        <Workshop/>
         <ContactUs />
       </div>
     );
