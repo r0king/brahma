cfadd3ad9a98360eebe2dd4f9e276c495fc8df58
17072001MUSAFIR <musafirky@gmail.com>
2023-02-20 19:43:33 +0530

    Hero Page Aligned

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,58 +1,59 @@
 import React from "react";
 import CountDown from "./CountDown";
-import logoAnimation from "../assets/images/diwali-lamp.gif";
+import logoAnimation from "../assets/images/brahmaLogo.png";
 export default function Hero() {
   return (
-    <section className="p-[5vw] pl-0 pt-[4vw] pr-0 overflow-hidden">
+    <section className="p-[5vw] pl-0 pt-[2vw] pr-0 ">
       <div className="relative pl-[5vw]">
-        <div className="pt-4">
-          <h1 className="text-sec-color font-morganite text-[25vw] pt-[5vh] md:pt-[5vw] md:text-[13vw] leading-[0.5] uppercase ">
+        <div className="pl-6 md:pl-0 pt-7 md:pt-4">
+          <h1 className="text-sec-color font-morganite text-[25vw] pt-[7vh] md:pt-[5vw] md:text-[13vw] leading-[0.5] uppercase ">
             Brahma '23
           </h1>
-          <h3 className="text-pri-color font-poppins font-semibold text-[5vw] md:text-[2.5vw] py-[1vh] md:py-0">
-            Awaken the Creator Within.
+          <h3 className="text-pri-color font-morganite font-semibold text-[25vw] md:text-[13vw] pt-3 md:pt-6 uppercase leading-[0.75]">
+            Awaken the
+            <br />Creator
+            <br />Within.
           </h3>
         </div>
         <div>
           <div
             className=" 
           hidden md:block  mr-[5vw] 
-          absolute top-[4vw] right-[4vw] w-[calc((135vw-9.375rem)*2/14+0.625rem)] border-b 
+          absolute top-[6vw] right-[4vw] w-[calc((135vw-9.375rem)*2/14+0.625rem)] border-b 
           after:bg-current after:rounded-full after:bottom-1.5 after:content-[''] after:h-[6px] after:overflow-hidden after:absolute after:right-0 after:aspect-square"
           >
             <p
               className="
           relative pb-2.5 pr-9
-          font-poppins font-semibold text-pri-color text-xs leading-none          
+          font-poppins font-semibold text-pri-color text-xs leading-4          
           "
             >
-              Fasten your seatbelts for the much anticipated and rousing event
-              of all times Brahma'23 is here. Get ready and prepare to see a
-              fantastic assortment of captivating events. Brace Yourself for the
-              Magic to come!
+              Get ready to unleash your inner genius and ignite your passion for learning, as our Brahma'23 event promises to be a one-of-a-kind experience that will inspire, educate, and entertain you like never before!
             </p>
           </div>
-          <div className="flex justify-center w-full mt-[-10vh] md:mt-0 ">
-            <div className="md:absolute top-[8vw] aspect-auto left-[10vw] w-full z-10 md:w-[38vw]">
+          <div className="flex justify-center w-full mt-[-25vh] md:mt-0 pr-3 md:pr-0">
+            <div className="md:absolute top-[4vw] aspect-auto md:left-[22.5vw] z-10 w-[50vw] md:w-[32vw]">
               <img src={logoAnimation} alt="logo" className="w-full h-full" />
             </div>
-          </div>
-          <div className="mt-[-9vh] mr-[2vw] md:mt-[4vw] pr-[4vw]">
-            <h1 className="text-right text-pri-color font-morganite text-[25vw] md:text-[13vw] leading-none uppercase ">
-              Mar.28-Apr.01
-            </h1>
-          </div>
-          <div className="flex justify-end mt-[-2vw] md:-mt-[1.5vw] flex-col md:flex-row pr-[6vw] md:pr-0">
-            <h1
-              className="text-pri-color text-right mr-[2vw] md:mr-[10vw] font-morganite text-[25vw] md:text-[13vw] leading-[0.75] uppercase "
-              style={{
-                marginInline: "0",
-              }}
-            >
-              @ASIET
-            </h1>
-            <div className="relative right-0 left-0 mt-[5vh]">
-              <CountDown />
+            <div className="absolute top-[70vh] md:top-[40vh] right-10 md:right-0">
+              <div className="">
+                <h1 className="text-right text-pri-color font-morganite text-[25vw] md:text-[13vw] leading-none uppercase ">
+                  Mar.28-Apr.01
+                </h1>
+              </div>
+              <div className="absolute top-[-6vh] md:top-[27vh] right-0 md:right-[25vw]">
+                <h1
+                  className="text-pri-color text-right mr-[2vw] md:mr-[10vw] font-morganite text-[25vw] md:text-[13vw] leading-[0.5] uppercase "
+                  style={{
+                    marginInline: "0",
+                  }}
+                >
+                  @ASIET
+                </h1>
+                <div className="absolute -right-[0vw] md:-right-[25vw] md:-top-1 pt-[12vh] md:pt-[2vh]">
+                  <CountDown />
+                </div>
+              </div>
             </div>
           </div>
         </div>
