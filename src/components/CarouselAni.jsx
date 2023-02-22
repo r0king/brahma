5331bc67633a5e9d97ebc59faa4bb97437011c0e
17072001MUSAFIR <musafirky@gmail.com>
2023-02-22 13:57:31 +0530

    Order Changed & Index.html Changed

--- src/components/CarouselAni.jsx
+++ src/components/CarouselAni.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import CardButton from "./CardButton";
 
 export default class CarouselAni extends Component {
   state = {
@@ -41,27 +42,29 @@ export default class CarouselAni extends Component {
 
   render() {
     return (
-      <div
-        className="marquee font-morganite text-[20vh] md:text-[35vh] opacity-90"
-        ref={this.marqueeRef}
-      >
-        <ul className="marquee__content uppercase">
-          <li>&nbsp;live Soon&nbsp;</li>
-          <li>&nbsp;live Soon&nbsp;</li>
-          <li>&nbsp;live Soon&nbsp;</li>
-          <li>&nbsp;live Soon&nbsp;</li>
-          <li>&nbsp;live Soon&nbsp;</li>
-          <li>&nbsp;live Soon&nbsp;</li>
-        </ul>
-        <ul className="marquee__content uppercase" aria-hidden="true">
-          <li>&nbsp;live Soon&nbsp;</li>
-          <li>&nbsp;live Soon&nbsp;</li>
-          <li>&nbsp;live Soon&nbsp;</li>
-          <li>&nbsp;live Soon&nbsp;</li>
-          <li>&nbsp;live Soon&nbsp;</li>
-          <li>&nbsp;live Soon&nbsp;</li>
-        </ul>
-      </div>
+      <>
+        <div
+          className="marquee font-morganite text-[20vh] md:text-[35vh] opacity-90"
+          ref={this.marqueeRef}
+        >
+          <ul className="marquee__content uppercase">
+            <li>&nbsp;live Soon&nbsp;</li>
+            <li>&nbsp;live Soon&nbsp;</li>
+            <li>&nbsp;live Soon&nbsp;</li>
+            <li>&nbsp;live Soon&nbsp;</li>
+            <li>&nbsp;live Soon&nbsp;</li>
+            <li>&nbsp;live Soon&nbsp;</li>
+          </ul>
+          <ul className="marquee__content uppercase" aria-hidden="true">
+            <li>&nbsp;live Soon&nbsp;</li>
+            <li>&nbsp;live Soon&nbsp;</li>
+            <li>&nbsp;live Soon&nbsp;</li>
+            <li>&nbsp;live Soon&nbsp;</li>
+            <li>&nbsp;live Soon&nbsp;</li>
+            <li>&nbsp;live Soon&nbsp;</li>
+          </ul>
+        </div>
+      </>
     );
   }
 }
