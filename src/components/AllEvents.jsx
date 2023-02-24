8d8dc514fccc4177532f29327039b30dfc7627bc
josephgeorgepadayatty <josephgeorgepadayatty@gmail.com>
2023-02-24 22:51:11 +0530

    fixed spacing

--- src/components/AllEvents.jsx
+++ src/components/AllEvents.jsx
@@ -8,151 +8,129 @@ import P6 from "../assets/images/pic6.jpeg";
 import P7 from "../assets/images/pic7.jpeg";
 import CardButton from './CardButton'
 const AllEvents = () => {
-    const images = [
-        {
-          name: "Bike Engine Assembly",
-          src: P1,
-          prize: "200",
-          organizer: "Pablo Stanley",
-          place: "Buenos",
-          state: "Mexico",
-        },
-        {
-          name: "Bike Engine Assembly",
-          src: P2,
-          prize: "200",
-          organizer: "Pablo Stanley",
-          place: "Buenos",
-          state: "Mexico",
-        },
-        {
-          name: "Bike Engine Assembly",
-          src: P3,
-          prize: "200",
-          organizer: "Pablo Stanley",
-          place: "Buenos",
-          state: "Mexico",
-        },
-        {
-          name: "Bike Engine Assembly",
-          src: P4,
-          prize: "200",
-          organizer: "Pablo Stanley",
-          place: "Buenos",
-          state: "Mexico",
-        },
-        {
-          name: "Bike Engine Assembly",
-          src: P5,
-          prize: "200",
-          organizer: "Pablo Stanley",
-          place: "Buenos",
-          state: "Mexico",
-        },
-        {
-          name: "Bike Engine Assembly",
-          src: P6,
-          prize: "200",
-          organizer: "Pablo Stanley",
-          place: "Buenos",
-          state: "Mexico",
-        },
-        {
-          name: "Bike Engine Assembly",
-          src: P7,
-          prize: "200",
-          organizer: "Pablo Stanley",
-          place: "Buenos",
-          state: "Mexico",
-        },
-      ];
-    return (
-        <div>
-            <div className="flex mt-[8vw] ml-[16vw] md:hidden">
-            <div className='absolute top-[24.2vw] left-0 text-lg font-bold font-poppins text-sec-color'>
-                    (+30)
-                </div>
-          <h4
-            className=" text-sec-color self-end font-semibold font-poppins text-lg 
-           w-full leading-[0.9] tracking-tighter p-2  pb-[5vw]  mr-auto"
-          >
-            Roll Up Your Sleeves For Practical Learning, Based On Real Projects From Industry Pros. 
-            Meet Like-Minded Classmates & Upskill Your Creative Toolkit.
-          </h4>
+  const images = [
+    {
+      name: "Bike Engine Assembly",
+      src: P1,
+      prize: "200",
+      organizer: "Pablo Stanley",
+      place: "Buenos",
+      state: "Mexico",
+    },
+    {
+      name: "Bike Engine Assembly",
+      src: P2,
+      prize: "200",
+      organizer: "Pablo Stanley",
+      place: "Buenos",
+      state: "Mexico",
+    },
+    {
+      name: "Bike Engine Assembly",
+      src: P3,
+      prize: "200",
+      organizer: "Pablo Stanley",
+      place: "Buenos",
+      state: "Mexico",
+    },
+    {
+      name: "Bike Engine Assembly",
+      src: P4,
+      prize: "200",
+      organizer: "Pablo Stanley",
+      place: "Buenos",
+      state: "Mexico",
+    },
+    {
+      name: "Bike Engine Assembly",
+      src: P5,
+      prize: "200",
+      organizer: "Pablo Stanley",
+      place: "Buenos",
+      state: "Mexico",
+    },
+    {
+      name: "Bike Engine Assembly",
+      src: P6,
+      prize: "200",
+      organizer: "Pablo Stanley",
+      place: "Buenos",
+      state: "Mexico",
+    },
+    {
+      name: "Bike Engine Assembly",
+      src: P7,
+      prize: "200",
+      organizer: "Pablo Stanley",
+      place: "Buenos",
+      state: "Mexico",
+    },
+  ];
+  return (
+    <div>
+      <div className="flex mt-[8vw] ml-[16vw] md:hidden">
+        <div className='absolute top-[24.8vw] left-0 text-lg font-bold font-poppins text-sec-color'>
+          (+30)
         </div>
-           
-            <div className='justify-between md:flex'>
-                
-                <div className=' font-morganite md:static text-pri-color text-[40vw] md:text-[26vw] pl-1 md:pt-[8%]'>
-                    <div className='leading-[0.75]'>ALL</div>
-                    <div className='leading-[0.75]'>EVENTS</div>
-                </div>
-                <div className=" hidden mb-[18vw] ml-[12vw]  mt-[5vw] md:flex">
+        <h4
+          className=" text-sec-color self-end font-semibold font-poppins text-lg 
+           w-full leading-[0.9] tracking-tighter p-2  pb-[5vw]  mr-auto"
+        >
+          Roll Up Your Sleeves For Practical Learning, Based On Real Projects From Industry Pros.
+          Meet Like-Minded Classmates & Upskill Your Creative Toolkit.
+        </h4>
+      </div>
+
+      <div className='justify-between md:flex'>
+
+        <div className=' font-morganite md:static text-pri-color text-[40vw] md:text-[26vw] pl-1 md:pt-[8%]'>
+          <div className='leading-[0.75]'>ALL</div>
+          <div className='leading-[0.75]'>EVENTS</div>
+        </div>
+        <div className=" hidden mb-[18vw] ml-[12vw]  mt-[5vw] md:flex">
           <h4
-            className="mb-[8vw]  text-sec-color self-end font-semibold font-poppins text-2xl md:text-[2.14rem] 
+            className="mb-[12.8vw]  text-sec-color self-end font-semibold font-poppins text-2xl md:text-[2.14rem] 
            w-full leading-[0.9] tracking-tighter p-2 md:p-0 md:w-[calc((100vw-9.375rem)*4/14+2.5rem)] pb-[5vw] md:ml-3 md:leading-none mr-auto"
           >
             Roll Up Your Sleeves For Practical Learning, Based On Real Projects From Industry Pros. Meet Like-Minded Classmates & Upskill Your Creative Toolkit.
           </h4>
           <div className='font-poppins absolute text-sec-color  text-[3rem] font-bold  right-0'>
-                    (+30)
-                </div>
+            (+30)
+          </div>
         </div>
-                <div className='hidden mr-6 w-[28vw] h-[8vw] md:inline mt-[38vw]'>
-                    <CardButton href='/' text={{ head: "Get", tail: "Slots", caption: "Registration" }} />
+        <div className='hidden mr-6 w-[28vw] h-[8vw] md:inline mt-[38vw] mb-[6vw]'>
+          <CardButton href='/' text={{ head: "Get", tail: "Slots", caption: "Registration" }} />
+        </div>
+      </div>
+      <div className='mt-[2vw] h-full w-full md:hidden'>
+          <CardButton href='/' text={{ head: "Get", tail: "Slots", caption: "Registration" }} />
+        </div>
+      <div className="mt-[5vw] grid max-w-screen-xl grid-cols-1 gap-4 mx-auto my-5 md:gap-y-20 justify-items-center md:grid-cols-2 ">
+        {images.map((image, index) => (
+          <div key={index}>
+            <div className="flex flex-col">
+              <img
+                className="rounded-tr-[4em] h-[50vw] sm:h-[30vw] md:h-[30vw] md:max-h-[40vh] md:w-[50vw] px-1"
+                src={image.src}
+                alt=''
+              />
+              <div className="pl-2 mt-4 text-lg font-bold font-poppins">
+                {image.name}
+              </div>
+              <div className="flex flex-row pl-2 mt-4 font-semibold">
+                <div className="w-12 pl-1 text-sm border-2 border-orange-500 font-poppins md:h-6">
+                  ₹{image.prize}
                 </div>
-            </div>
-            <div className='mt-[2vw] relative mx-2 overflow-hidden transition-all bg-pri-color rounded-lg group h-1/3 md:hidden'>
-                <span className="absolute top-0 bottom-0 w-full h-full transition-all duration-300 ease-out rounded-lg bg-sec-color group-hover:h-0 -z-0" />
-                <span className='relative z-10 flex justify-between w-full p-2 text-white transition-colors duration-100 ease-in-out rounded-lg stroke-white '>
-                    <div className='text-left'>
-                        <label className='mb-2 text-xs font-medium'>Registration</label>
-                        <div className='flex flex-col items-stretch mt-4 text-2xl font-semibold'>
-                            <label className=''>Get</label>
-                            <label className=''>Slots</label>
-                        </div>
-                    </div>
-                    <svg
-                        className="absolute bottom-0 right-0 m-2"
-                        width="34px"
-                        height="34px"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <g id="SVGRepo_bgCarrier" strokeWidth={0} />
-                        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
-                        <g id="SVGRepo_iconCarrier">
-                            <path d="M16.2928932,7 L11.5,7 C11.2238576,7 11,6.77614237 11,6.5 C11,6.22385763 11.2238576,6 11.5,6 L17.5,6 C17.7761424,6 18,6.22385763 18,6.5 L18,12.5 C18,12.7761424 17.7761424,13 17.5,13 C17.2238576,13 17,12.7761424 17,12.5 L17,7.70710678 L6.85355339,17.8535534 C6.65829124,18.0488155 6.34170876,18.0488155 6.14644661,17.8535534 C5.95118446,17.6582912 5.95118446,17.3417088 6.14644661,17.1464466 L16.2928932,7 Z" />{" "}
-                        </g>
-                    </svg>
-                </span>
-            </div>
-            <div className="mt-[5vw] grid max-w-screen-xl grid-cols-1 gap-4 mx-auto my-5 gap-y-6 justify-items-center md:grid-cols-2 ">
-            {images.map((image, index) => (
-            <div key={index}>
-              <div className="flex flex-col">
-                <img
-                  className="rounded-tr-[4em] h-[50vw] sm:h-[30vw] md:h-[30vw] md:max-h-[40vh] md:w-[50vw] px-1"
-                  src={image.src}
-                  alt=''
-                />
-                <div className="pl-2 mt-4 text-lg font-bold font-poppins">
-                  {image.name}
-                </div>
-                <div className="flex flex-row pl-2 mt-4 font-semibold">
-                  <div className="w-12 pl-1 text-sm border-2 border-orange-500 font-poppins md:h-6">
-                    ₹{image.prize}
-                  </div>
-                  <div className="ml-6 font-poppins">{image.organizer}</div>
-                  <div className="ml-6 font-poppins">{image.place}</div>
-                  <div className="ml-6 font-poppins">{image.state}</div>
-                </div>
+                <div className="ml-6 font-poppins">{image.organizer}</div>
+                <div className="ml-6 font-poppins">{image.place}</div>
+                <div className="ml-6 font-poppins">{image.state}</div>
               </div>
             </div>
-          ))}
-            </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default AllEvents;
--- src/components/SpotLight.jsx
+++ src/components/SpotLight.jsx
@@ -1,72 +1,69 @@
 import React from "react";
 
 const SpotLight = () => {
-  const imgStyle = {
-    boxShadow: "inset 10px 10px 40px #d35c39",
-  };
 
   return (
-    <div className="grid w-screen mr-6 md:w-auto sm: place-items-center">
+    <div className="grid w-full mr-6 md:w-auto sm: place-items-center">
       <div className="flex flex-col ">
         <div className="">
           <div className="flex justify-end rounded-lg">
-            <span className="text-7xl font-bold font-morganite text-sec-color md:text-[12vw] mt-[0.5vw]">
+            <span className="text-7xl font-bold font-morganite text-sec-color md:text-[12vw] mt-[0.7vw]">
               36
             </span>
             <span className="leading-[0.88] text-[150px] font-medium tracking-normal font-morganite text-pri-color uppercase md:text-[31vw]">
               TALKS
             </span>
-
-            <img
-              className="hidden object-cover  ml-4 rounded-lg h-[21.5vw] w-96 md:inline mt-[2.8vw]"
-              src="https://source.unsplash.com/1200x800/?3"
-              alt="talks"
-            />
-
+            <div className='relative'>
+              <img
+                className="hidden object-cover drop-shadow-md ml-4 rounded-lg h-[21.5vw] w-96 md:inline mt-[2.8vw]"
+                src="https://source.unsplash.com/1200x800/?3"
+                alt="talks"
+              />
+              <div className='absolute inset-0 rounded-lg shadow-overlay h-[21.5vw] mt-[2.8vw] ml-4 opacity-40'></div>
+            </div>
           </div>
         </div>
-        <div className="">
+        <div className="-mt-20">
           <div className="flex flex-row-reverse justify-start rounded-lg">
-            <span className="font-bold text-7xl font-morganite text-sec-color md:text-[12vw] ">
+            <span className="font-bold text-7xl font-morganite text-sec-color md:text-[12vw] mt-[1vw] ">
               6
             </span>
             <span className="leading-[0.88] text-[150px] font-medium font-morganite tracking-normal text-pri-color uppercase md:text-[31vw]">
               WORKSHOPS
             </span>
-            <img
-              className="hidden object-cover h-[21.5vw] mr-4 rounded-lg w-80 md:inline mt-[2.8vw]"
-              src="https://source.unsplash.com/1200x800/?2"
-              style={imgStyle}
-              alt="workshop"
-            />
+            <div className='relative'>
+              <img
+                className="hidden object-cover drop-shadow-md ml-4 rounded-lg h-[21.5vw] w-[18vw] mr-4 md:inline mt-[2.8vw]"
+                src="https://source.unsplash.com/1200x800/?2"
+                alt="talks"
+              />
+              <div className='absolute inset-0 rounded-lg shadow-overlay h-[21.5vw] mt-[2.8vw] ml-4 mr-4 opacity-40'></div>
+            </div>
           </div>
         </div>
-        <div className="">
+        <div className="-mt-20">
           <div className="flex justify-end rounded-lg ">
-            <span className="font-bold text-7xl font-morganite text-sec-color md:text-[12vw] ">
+            <span className="font-bold text-7xl font-morganite text-sec-color md:text-[12vw] mt-[1vw] ">
               36
             </span>
             <span className="leading-[0.88] text-[150px] font-morganite font-medium tracking-normal text-pri-color uppercase md:text-[31vw]">
               SPEAKERS
             </span>
-            <img
-              className="hidden object-cover h-[21.5vw] ml-4 rounded-lg w-96 md:inline mt-[2.8vw]"
-              src="https://source.unsplash.com/1200x800/?1"
-              style={imgStyle}
-              alt="speakers"
-            />
+            <div className='relative'>
+              <img
+                className="hidden object-cover drop-shadow-md ml-4 rounded-lg h-[21.5vw] w-96 md:inline mt-[2.8vw]"
+                src="https://source.unsplash.com/1200x800/?1"
+                alt="talks"
+              />
+              <div className='absolute inset-0 rounded-lg shadow-overlay h-[21.5vw] mt-[2.8vw] ml-4 opacity-40'></div>
+            </div>
           </div>
         </div>
       </div>
     </div>
+  
 
   );
 };
 
 export default SpotLight;
-
-
-
-
-
-
