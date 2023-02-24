6ba5db739b7a484827a28b2ca7cc3252615f64dd
josephgeorgepadayatty <josephgeorgepadayatty@gmail.com>
2023-02-24 21:32:06 +0530

    changed images and added description

--- src/components/AllEvents.jsx
+++ src/components/AllEvents.jsx
@@ -1,9 +1,85 @@
 import React from 'react'
-import Event1 from "../assets/images/event2.jpg"
+import P1 from "../assets/images/pic1.jpeg";
+import P2 from "../assets/images/pic2.jpeg";
+import P3 from "../assets/images/pic3.jpeg";
+import P4 from "../assets/images/pic4.jpg";
+import P5 from "../assets/images/pic5.jpeg";
+import P6 from "../assets/images/pic6.jpeg";
+import P7 from "../assets/images/pic7.jpeg";
 import CardButton from './CardButton'
 const AllEvents = () => {
+    const images = [
+        {
+          name: "Bike Engine Assembly",
+          src: P1,
+          prize: "200",
+          organizer: "Pablo Stanley",
+          place: "Buenos",
+          state: "Mexico",
+        },
+        {
+          name: "Bike Engine Assembly",
+          src: P2,
+          prize: "200",
+          organizer: "Pablo Stanley",
+          place: "Buenos",
+          state: "Mexico",
+        },
+        {
+          name: "Bike Engine Assembly",
+          src: P3,
+          prize: "200",
+          organizer: "Pablo Stanley",
+          place: "Buenos",
+          state: "Mexico",
+        },
+        {
+          name: "Bike Engine Assembly",
+          src: P4,
+          prize: "200",
+          organizer: "Pablo Stanley",
+          place: "Buenos",
+          state: "Mexico",
+        },
+        {
+          name: "Bike Engine Assembly",
+          src: P5,
+          prize: "200",
+          organizer: "Pablo Stanley",
+          place: "Buenos",
+          state: "Mexico",
+        },
+        {
+          name: "Bike Engine Assembly",
+          src: P6,
+          prize: "200",
+          organizer: "Pablo Stanley",
+          place: "Buenos",
+          state: "Mexico",
+        },
+        {
+          name: "Bike Engine Assembly",
+          src: P7,
+          prize: "200",
+          organizer: "Pablo Stanley",
+          place: "Buenos",
+          state: "Mexico",
+        },
+      ];
     return (
         <div>
+            <div className="flex mt-[8vw] ml-[16vw] md:hidden">
+            <div className='absolute top-[24.2vw] left-0 text-lg font-bold font-poppins text-sec-color'>
+                    (+30)
+                </div>
+          <h4
+            className=" text-sec-color self-end font-semibold font-poppins text-lg 
+           w-full leading-[0.9] tracking-tighter p-2  pb-[5vw]  mr-auto"
+          >
+            Roll Up Your Sleeves For Practical Learning, Based On Real Projects From Industry Pros. 
+            Meet Like-Minded Classmates & Upskill Your Creative Toolkit.
+          </h4>
+        </div>
            
             <div className='justify-between md:flex'>
                 
@@ -11,20 +87,23 @@ const AllEvents = () => {
                     <div className='leading-[0.75]'>ALL</div>
                     <div className='leading-[0.75]'>EVENTS</div>
                 </div>
-                <div className=" hidden mb-[18vw] ml-[16vw] md:inline mt-[5vw]">
+                <div className=" hidden mb-[18vw] ml-[16vw]  mt-[5vw] md:flex">
           <h4
             className=" text-sec-color self-end font-semibold font-poppins text-2xl md:text-[2.14rem] 
            w-full leading-[0.9] tracking-tighter p-2 md:p-0 md:w-[calc((100vw-9.375rem)*4/14+2.5rem)] pb-[5vw] md:ml-3 md:leading-none mr-auto"
           >
             Roll Up Your Sleeves For Practical Learning, Based On Real Projects From Industry Pros. Meet Like-Minded Classmates & Upskill Your Creative Toolkit.
           </h4>
+          <div className='font-poppins absolute text-sec-color  text-[2.75rem] font-bold  right-0'>
+                    (+30)
+                </div>
         </div>
-                <div className='hidden mr-24 w-1/4 h-[8vw] md:inline mt-[28vw]'>
+                <div className='hidden mr-6 w-[28vw] h-[8vw] md:inline mt-[28vw]'>
                     <CardButton href='/' text={{ head: "Get", tail: "Slots", caption: "Registration" }} />
                 </div>
             </div>
-            <div className='mt-[2vw] relative mx-2 overflow-hidden transition-all bg-red-600 rounded-lg group h-1/3 md:hidden'>
-                <span className="absolute top-0 bottom-0 w-full h-full transition-all duration-300 ease-out bg-orange-500 rounded-lg group-hover:h-0 -z-0" />
+            <div className='mt-[2vw] relative mx-2 overflow-hidden transition-all bg-sec-color rounded-lg group h-1/3 md:hidden'>
+                <span className="absolute top-0 bottom-0 w-full h-full transition-all duration-300 ease-out rounded-lg bg-sec-color group-hover:h-0 -z-0" />
                 <span className='relative z-10 flex justify-between w-full p-2 text-white transition-colors duration-100 ease-in-out rounded-lg stroke-white '>
                     <div className='text-left'>
                         <label className='mb-2 text-xs font-medium'>Registration</label>
@@ -49,64 +128,28 @@ const AllEvents = () => {
                 </span>
             </div>
             <div className="mt-[5vw] grid max-w-screen-xl grid-cols-1 gap-4 mx-auto my-5 gap-y-6 justify-items-center md:grid-cols-2 ">
-                <div className='relative  h-[50vh] md:h-[65vh] w-[70%] md:w-[80%] rounded-tr-[15%] bg-pri-color hover:bg-gradient-to-t hover:from-sec-color hover:to-pri-color md:ml-[8.5vw]'>
-                    <img src={Event1} alt="" className='filter drop-shadow-2xl brightness-50 opacity-60 w-full h-full object-fill rounded-tr-[15%]' />
-                    <div className='absolute bottom-0 pb-1 md:pb-3'>
-                        <div className='pl-1 font-semibold text-acc-color md:pl-4 font-poppins text-md md:text-3xl'>MERCADO JUEGO</div>
-                        <div className='pl-1 text-sm text-acc-color md:pl-4 font-poppins md:text-xl'>Venue</div>
-                    </div>
-                    <div className='absolute bottom-0 right-0 p-1 text-sm text-acc-color md:pb-3 md:pr-4 font-poppins md:text-xl'>Date & Time</div>
+            {images.map((image, index) => (
+            <div key={index}>
+              <div className="flex flex-col">
+                <img
+                  className="rounded-tr-[4em] h-[50vw] sm:h-[30vw] md:h-[30vw] md:max-h-[40vh] md:w-[50vw] px-1"
+                  src={image.src}
+                  alt=''
+                />
+                <div className="pl-2 mt-4 text-lg font-bold font-poppins">
+                  {image.name}
                 </div>
-
-
-                <div className='relative  h-[50vh] md:h-[65vh] w-[70%] md:w-[80%]  rounded-tr-[15%] bg-pri-color hover:bg-gradient-to-t hover:from-sec-color hover:to-pri-color md:mr-[8.5vw] '>
-                    <img src={Event1} alt="" className='filter drop-shadow-2xl brightness-50 opacity-60 w-full h-full object-fill rounded-tr-[15%]' />
-                    <div className='absolute bottom-0 pb-1 md:pb-3'>
-                        <div className='pl-1 font-semibold text-acc-color md:pl-4 font-poppins text-md md:text-3xl'>MERCADO JUEGO</div>
-                        <div className='pl-1 text-sm text-acc-color md:pl-4 font-poppins md:text-xl'>Venue</div>
-                    </div>
-                    <div className='absolute bottom-0 right-0 p-1 text-sm text-acc-color md:pb-3 md:pr-4 font-poppins md:text-xl'>Date & Time</div>
+                <div className="flex flex-row pl-2 mt-4 font-semibold">
+                  <div className="w-12 pl-1 text-sm border-2 border-orange-500 font-poppins md:h-6">
+                    ₹{image.prize}
+                  </div>
+                  <div className="ml-6 font-poppins">{image.organizer}</div>
+                  <div className="ml-6 font-poppins">{image.place}</div>
+                  <div className="ml-6 font-poppins">{image.state}</div>
                 </div>
-                <div className='relative h-[50vh] md:h-[65vh] w-[70%] md:w-[80%]  rounded-tr-[15%] bg-pri-color hover:bg-gradient-to-t hover:from-sec-color hover:to-pri-color md:ml-[8.5vw]'>
-                    <img src={Event1} alt="" className='filter drop-shadow-2xl brightness-50 opacity-60 w-full h-full object-fill rounded-tr-[15%]' />
-                    <div className='absolute bottom-0 pb-1 md:pb-3'>
-                        <div className='pl-1 font-semibold text-acc-color md:pl-4 font-poppins text-md md:text-3xl'>MERCADO JUEGO</div>
-                        <div className='pl-1 text-sm text-acc-color md:pl-4 font-poppins md:text-xl'>Venue</div>
-                    </div>
-                    <div className='absolute bottom-0 right-0 p-1 text-sm text-acc-color md:pb-3 md:pr-4 font-poppins md:text-xl'>Date & Time</div>
-                </div>
-                <div className='relative h-[50vh] md:h-[65vh] w-[70%] md:w-[80%]  rounded-tr-[15%] bg-pri-color hover:bg-gradient-to-t hover:from-sec-color hover:to-pri-color md:mr-[8.5vw]'>
-                    <img src={Event1} alt="" className='filter drop-shadow-2xl brightness-50 opacity-60 w-full h-full object-fill rounded-tr-[15%]' />
-                    <div className='absolute bottom-0 pb-1 md:pb-3'>
-                        <div className='pl-1 font-semibold text-acc-color md:pl-4 font-poppins text-md md:text-3xl'>MERCADO JUEGO</div>
-                        <div className='pl-1 text-sm text-acc-color md:pl-4 font-poppins md:text-xl'>Venue</div>
-                    </div>
-                    <div className='absolute bottom-0 right-0 p-1 text-sm text-acc-color md:pb-3 md:pr-4 font-poppins md:text-xl'>Date & Time</div>
-                </div>
-                <div className='relative h-[50vh] md:h-[65vh] w-[70%] md:w-[80%]  rounded-tr-[15%] bg-pri-color hover:bg-gradient-to-t hover:from-sec-color hover:to-pri-color md:ml-[8.5vw]'>
-                    <img src={Event1} alt="" className='filter drop-shadow-2xl brightness-50 opacity-60 w-full h-full object-fill rounded-tr-[15%]' />
-                    <div className='absolute bottom-0 pb-1 md:pb-3'>
-                        <div className='pl-1 font-semibold text-acc-color md:pl-4 font-poppins text-md md:text-3xl'>MERCADO JUEGO</div>
-                        <div className='pl-1 text-sm text-acc-color md:pl-4 font-poppins md:text-xl'>Venue</div>
-                    </div>
-                    <div className='absolute bottom-0 right-0 p-1 text-sm text-acc-color md:pb-3 md:pr-4 font-poppins md:text-xl'>Date & Time</div>
-                </div>
-                <div className='relative h-[50vh] md:h-[65vh] w-[70%] md:w-[80%]  rounded-tr-[15%] bg-pri-color hover:bg-gradient-to-t hover:from-sec-color hover:to-pri-color md:mr-[8.5vw]'>
-                    <img src={Event1} alt="" className='filter drop-shadow-2xl brightness-50 opacity-60 w-full h-full object-fill rounded-tr-[15%]' />
-                    <div className='absolute bottom-0 pb-1 md:pb-3'>
-                        <div className='pl-1 font-semibold text-acc-color md:pl-4 font-poppins text-md md:text-3xl'>MERCADO JUEGO</div>
-                        <div className='pl-1 text-sm text-acc-color md:pl-4 font-poppins md:text-xl'>Venue</div>
-                    </div>
-                    <div className='absolute bottom-0 right-0 p-1 text-sm text-acc-color md:pb-3 md:pr-4 font-poppins md:text-xl'>Date & Time</div>
-                </div>
-                <div className='relative h-[50vh] md:h-[65vh] w-[70%] md:w-[80%]  rounded-tr-[15%] bg-pri-color hover:bg-gradient-to-t hover:from-sec-color hover:to-pri-color md:ml-[8.5vw]'>
-                    <img src={Event1} alt="" className='filter drop-shadow-2xl brightness-50 opacity-60 w-full h-full object-fill rounded-tr-[15%]' />
-                    <div className='absolute bottom-0 pb-1 md:pb-3'>
-                        <div className='pl-1 font-semibold text-acc-color md:pl-4 font-poppins text-md md:text-3xl'>MERCADO JUEGO</div>
-                        <div className='pl-1 text-sm text-acc-color md:pl-4 font-poppins md:text-xl'>Venue</div>
-                    </div>
-                    <div className='absolute bottom-0 right-0 p-1 text-sm text-acc-color md:pb-3 md:pr-4 font-poppins md:text-xl'>Date & Time</div>
-                </div>
+              </div>
+            </div>
+          ))}
             </div>
         </div>
     );
