b8732d8f334c6f04df06bc689e48c9d6c82243bd
17072001MUSAFIR <musafirky@gmail.com>
2023-02-24 23:32:39 +0530

    Highlight responsive 01

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -21,7 +21,7 @@ const Highlights = () => {
                 <p className='md:basis-1/5 order-2 md:order-1 ml-[10vw] md:ml-[10vw] md:w-[1vw] mr-[4vw] md:mx-0 mt-[20vw] md:mt-[3vw] font-poppins font-semibold'>
                     We return with the digital design community stronger than ever. Talks, networking, activities, and parties. Learn from global experts, connect with like-minded professionals, and move the industry forward together.
                 </p>
-                <h1 className='uppercase font-morganite text-[50vw] md:text-[22vw] leading-[0.5] md:leading-[0.4] ml-[45vw] md:ml-[10vw] mt-[5vw] md:mt-0 order-1 md:order-2'>US</h1>
+                <h1 className='uppercase font-morganite text-[50vw] md:text-[22vw] leading-[0.5] ml-[45vw] md:ml-[10vw] mt-[5vw] md:mt-0 order-1 md:order-2'>US</h1>
                 <div className='basis-1/3 order-3 h-[10vw] mt-[10vw] md:mt-0'>
                     <CardButton text={{ head: "Get", tail: "Tickets", caption: "Tickets" }} />
                 </div>
